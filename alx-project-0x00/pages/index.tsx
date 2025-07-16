cd1ba85ee1fb8980431c4b4bebc0a060838cce81
RachidJedata <jedatarachid@gmail.com>
2025-07-16 16:59:02 +0100

    Setting up project directories

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -11,7 +11,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const Home = () => {
+const Home: React.FC = () => {
   return (
     <div
       className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
